c6fc365529cb7941fe774d06ae1f2f3edabfc500
Dennis Kimaita <dkimaita22@gmail.com>
2025-05-19 11:42:13 +0300

    wrap components in storeContext and fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/properties" element={<Properties />} />
-        <Route path="/properties/:id" element={<PropertyDetail />} />
+        <Route path="/properties/:propertyId" element={<PropertyDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
