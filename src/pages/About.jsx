a17f174d98924ea415bb7f8bbaa1cb2f215218a7
Dennis Kimaita <dkimaita22@gmail.com>
2025-05-23 11:23:09 +0300

    autoplay video social meddia manager details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -64,7 +64,7 @@ export default function About() {
                   autoPlay
                   muted
                   playsInline
-                  preload="metadata"
+                  preload="auto"
                   // poster="/video-poster.jpg" // Recommended for better UX
                 />
               </div>
